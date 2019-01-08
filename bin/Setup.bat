31d570f35c4a26e8ab160e4e877afabee964fe71
Masoud A. Moghaddam <masoud.mghd@gmail.com>
2019-01-08 16:08:09 +0330

    Add files via upload

---
+++ bin/Setup.bat
@@ -0,0 +1,5 @@
+@echo off
+xcopy VbaProject.OTM %userprofile%\AppData\Roaming\Microsoft\Outlook /s/h/e/k/f/c/y
+xcopy olkexplorer.officeUI %userprofile%\AppData\Local\Microsoft\Office /s/h/e/k/f/c/y
+xcopy quote.xlsx %userprofile%\AppData\Roaming\Microsoft\Signatures /s/h/e/k/f/c/y
+exit
